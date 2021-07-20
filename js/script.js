4fcdd1776448120b0b4a874b849939dd1644dbe7
cwhyman <cwhyma@gmail.com>
2021-07-20 23:02:46 +0100

    Added functionality to update calculator window when number keys are pressed

--- js/script.js
+++ js/script.js
@@ -22,3 +22,27 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 };
+
+//create a function to update the display if a number is pressed
+//attach event listeners to buttons
+function updateDisplay() {
+    const wrapper = document.getElementById('wrapper');
+    
+    wrapper.addEventListener('click', (event) => {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+            return;
+        }
+
+        console.log(event.target.className);
+
+        // if the class name is number, display the number in the results box
+        if(event.target.className === 'numbers') {
+            inputNumber = document.getElementById('result').value += event.target.value;
+            console.log(inputNumber);
+        };
+    });
+};
+
+updateDisplay();
+
