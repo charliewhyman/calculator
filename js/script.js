c9cef3e6c7abf91b30eb33a1eff7ba623180b25a
cwhyman <cwhyma@gmail.com>
2021-07-24 23:22:49 +0100

    Fixed calculation chain bug

--- js/script.js
+++ js/script.js
@@ -9,43 +9,67 @@ function operate(operator, num1, num2) {
     } else if (operator ==="divide") {
         return num1 / num2;
     }
-}
+};
+
+//create a function to clear the display
+function clearAll() {
+    document.getElementById('result').value = "";
+    let inputNumber = null;
+    let storedNumber = null;
+    let operatorClick = null;
+    console.log('inputNumber: ' + inputNumber);
+    console.log('storedNumber: ' + storedNumber);
+    console.log('operatorClick: ' + operatorClick);
+    updateDisplay();
+
+};
 
 //create a function to clear the display
 function clearDisplay() {
+    let inputNumber = null;
     document.getElementById('result').value = "";
-    let inputNumber = 0;
-}
+};
 
 //create a function to update the display if a number is pressed
 //attach event listeners to buttons
 function updateDisplay() {
     const wrapper = document.getElementById('wrapper');
-    let inputNumber = 0;
+    let inputNumber = null;
+    let storedNumber = null;
+    let operatorClick = null;
+
+// define a nested function to display the results on the calculator screen
+
+    function displayResults() {
+        return document.getElementById('result').value = storedNumber;
+    };
 
     wrapper.addEventListener('click', (event) => {
         const isButton = event.target.nodeName === 'BUTTON';
         if (!isButton) {
             return;
-        }
+        };
 
-// firstly, if a series of numbers are pressed, store and display it/them
-        if (event.target.className === 'numbers') {
+// firstly, if a series of numbers are pressed before an operator, store and display the number(s)
+        if (event.target.className === 'numbers' && operatorClick === null) {
             inputNumber = parseInt(document.getElementById('result').value += event.target.value);
-//then, if the user presses an operator, save the operator type in a variable
+            storedNumber = inputNumber;
+            displayResults();
+
+// if a number is pressed after an operator, store the new input number, then operate on the stored and input numbers to calculate the new stored number
+        } else if (event.target.className === 'numbers' && operatorClick !== null) {
+            inputNumber = parseInt(document.getElementById('result').value += event.target.value);
+            storedNumber = operate(operatorClick, storedNumber, inputNumber);
+            displayResults();
+
+// if an operator is pressed, record the operator id and clear the display
         } else if (event.target.className === 'operators') {
             operatorClick = event.target.id.toString();
-            //Store the new number(s) in a different variable and clear the display
-            storedNumber = inputNumber;
             clearDisplay();
-//otherwise if the user presses equals, run the operate function using the two numbers and operator        
-        } else if (event.target.className === 'equals') {
-            console.log(operatorClick);
-            console.log(inputNumber);
-            console.log(storedNumber);
 
-            resultNumber = operate(operatorClick, inputNumber, storedNumber);
-            document.getElementById('result').value = resultNumber;
+// if the equals button is pressed, show the latest calculation results
+        } else if (event.target.className === 'equals' && operatorClick !== null) {
+            displayResults();
         }
     });
 };
